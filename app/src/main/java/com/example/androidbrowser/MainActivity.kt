ea3bb6001b43a0ae54b96ddd3769ad3b5f66b6ca
a18507564 <PVMikhalev@sberbank.ru>
2020-06-24 18:20:49 +0300

    StartIntent added

--- app/src/main/java/com/example/androidbrowser/MainActivity.kt
+++ app/src/main/java/com/example/androidbrowser/MainActivity.kt
@@ -1,25 +1,53 @@
 package com.example.androidbrowser
 
+import android.annotation.SuppressLint
+import android.content.Intent.*
 import android.os.Bundle
 import android.view.KeyEvent
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val URL = "https://elitmedplace.ru/"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val url = "https://elitmedplace.ru/"
         webView = findViewById(R.id.webview)
-        webView.webViewClient = WebViewClient()
+
+        val context = this
+        val webViewClient = object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                return try {
+                    val intent = parseUri(url, URI_INTENT_SCHEME)
+                    startActivity(intent)
+                    true
+                } catch (ex: android.content.ActivityNotFoundException) {
+                    webview.stopLoading()
+                    webview.goBack()
+                    Toast.makeText(
+                        context,
+                        "Ошибка! Не удалось открыть приложение",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                    false
+                }
+            }
+        }
+        webView.webViewClient = webViewClient
         val webSettings = webView.settings
         webSettings.javaScriptEnabled = true
         supportActionBar!!.hide()
-        webView.loadUrl(url)
+        webView.loadUrl(URL)
     }
 
 
@@ -38,4 +66,5 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onKeyDown(keyCode, event)
     }
+
 }
